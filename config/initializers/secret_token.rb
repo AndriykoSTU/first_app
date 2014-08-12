3d0842421c423f72a799846a4cb11aa4e6f0c6ae
Andriyko <Balfagor@bigmir.net>
2014-08-12 03:30:38 +0300

    Инициализировать хранилище

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = '6f42237aad0bc1ae04f14fec118817d637d306314d598a4f3aa318bfd257f2674ed0eabdce7cb9565bf785457b3c96fca167bc8328bbd64950fe92167acb90b0'
